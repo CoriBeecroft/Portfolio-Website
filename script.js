5609feb4c433d076ced52eba44c4b33a5be6407d
Cori Beecroft <CoriBeecroft@users.noreply.github.com>
2020-01-13 21:10:57 -0700

    Updated content to include a 'quick update' about what I've been doing lately

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const PortfolioPage = () => {
         {[
             <Header />,
             <Intro />,
-            <Forty96 />,
+            <UpdatesComingSoon />,
             <HNFE />,
+            <Forty96 />,
             <NestingDocs />,
             <Lucidity />,
             <DoomPet />,
@@ -36,6 +37,105 @@ function Intro() {
             minor in computer science. This website is a place for me to
             share some of my programming projects.
         </p>
+	<span>
+            <a href="mailto:coribeecroft@gmail.com">E-mail</a>
+            { " | " }
+      	    <a href="https://github.com/coribeecroft" target="_blank">GitHub</a>
+            { " | " }
+            <a href="https://www.linkedin.com/in/cori-beecroft-9546446b/" target="_blank">LinkedIn</a>
+    	</span>
+    </div>
+}
+
+const UpdatesComingSoon = () => {
+    return <div>
+        <h2 className="col-lg-12">Quick Update</h2>
+        <section>
+            It has been a while since I've had a chance to update this site. 
+            I'm hoping to make some updates soon. Until then, here is a little
+            bit about what I've been up to recently.
+        </section>
+        <section>
+            <h3>Full Stack Development at NetToolKit</h3>
+            <ul>
+                <li>
+                    Developed an experimental CAPTCHA service (full stack)
+{/*                     <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+ */}                </li>
+                <li>
+                    Made a JavaScript map library that abstracts over Google Maps, Bing Maps and OpenLayers (front end)
+{/*                     <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+ */}                </li>
+                <li>
+                    Developed or contributed to lots of smaller projects including:
+                    <ul>
+                        <li>A tool for comparing geocoding providers (front end)</li>
+                        <li>Prototype for a somewhat unique software monitoring tool (full stack)</li>
+                        <li>Various internal libraries (mostly front end)</li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h3 style={{ display: "inline-block", marginRight: "5px" }}>Fireflies</h3>
+            <a href="../fireflies" target="_blank">(See Fireflies)</a>
+            <p>A graphical demo built on React. I made this for fun on a weekend.</p>
+            <ul>
+                <li>Click to add a new firefly</li>
+                <li>Click and hold to grow a new firefly</li>
+                <li>Click and hold for a while after the firefly stops growing for an Easter egg :)</li>
+                <li>Optionally specify number of fireflies in the query string using "numFireflies"</li>
+            </ul>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <a target="_blank" href="images/fireflies.png">
+                    <img { ...{
+                        className: "fireflies",
+                        title: "Fireflies",
+                        alt: "Fireflies",
+                        src: "images/fireflies.png",
+                    }} />
+                </a>
+            </div>
+        </section>
+        <section>
+            <h3>Code Tutor</h3>
+            <p>
+                Did front end work for a "code tutor" Electron app that I was 
+                working on with <a href="http://symbolflux.com">my brother</a>.
+            </p>
+            <div style={{
+                display: "flex",
+                flexWrap: "wrap"
+            }}>
+                <a target="_blank" href="images/code-tutor-wireframe.jpg">
+                    <img { ...{
+                        className: "code-tutor",
+                        title: "Code Tutor Wireframe",
+                        alt: "Code Tutor Wireframe",
+                        src: "images/code-tutor-wireframe.jpg",
+                    }} />
+                </a>
+                <div style={{ margin: "auto 10px" }}>&rarr;</div>
+                <a target="_blank" href="images/code-tutor.png">
+                    <img { ...{
+                        className: "code-tutor",
+                        title: "Code Tutor",
+                        alt: "Code Tutor",
+                        src: "images/code-tutor.png",
+                    }} />
+                </a>
+            </div>
+        </section>
     </div>
 }
 
@@ -44,7 +144,7 @@ const Forty96 = () => {
         <h2 className="col-lg-12">4096</h2>
         <h4>
             <a href="../4096/index.html" target="_blank">
-                (Click here to play 4096)
+                (Play 4096)
             </a>
         </h4>
         <img { ...{
@@ -103,7 +203,7 @@ const HNFE = () => {
         <h2 className="col-lg-12">Hacker News Front-End</h2>
         <h4>
             <a href="../hacker-news-frontend/index.html" target="_blank">
-                (Click here to see Hacker News front-end)
+                (See Hacker News front-end)
             </a>
         </h4>
         <img className="img-responsive" title="4096" alt="4096" src="images/HN.png" />
@@ -124,12 +224,6 @@ const HNFE = () => {
         </section> */}
         <section>
             <h3>TODO: </h3>
-            <span>
-                Note: I'm not actively working on this project anymore
-                since the main purpose of it was to learn React and what's
-                left to do won't contribute significantly to my
-                knowledge/understanding of React.
-            </span>
             <ul>
                 <li>Make a better mobile interface</li>
                 <li>Make it an infinite scroller</li>
@@ -137,11 +231,11 @@ const HNFE = () => {
                 <li>Make ask, show and job story lists (currently only top stories are shown)</li>
             </ul>
         </section>
-        <section>
+{        <section>
             <h3>Written With: </h3>
-            <p>JavaScript, React, jQuery HTML, CSS</p>
+            <p>JavaScript, React, CSS</p>
         </section>
-        <section>
+}        <section>
             <h3>GitHub: </h3>
             <a href="https://github.com/coribeecroft/hacker-news-frontend" target="_blank">
                 https://github.com/coribeecroft/hacker-news-frontend
@@ -156,7 +250,7 @@ const NestingDocs = () => {
             <h2 className="col-lg-12">Nesting Docs</h2>
             <h4>
                 <a href="http://coribeecroft.com/LODEssay" target="_blank">
-                    (Click here to see a nesting doc)
+                    (See a nesting doc)
                 </a>
             </h4>
         </div>
@@ -213,7 +307,7 @@ const Lucidity = () => {
             <h2 className="col-lg-12">Lucidity</h2>
             <h4>
                 <a href="http://symbolflux.com/projects/avd" target="_blank">
-                    (Click here for more information on Lucidity)
+                    (More information on Lucidity)
                 </a>
             </h4>
         </div>
@@ -327,7 +421,7 @@ const Footer = () => (<footer>
         { " | " }
         <a href="https://www.linkedin.com/in/cori-beecroft-9546446b/" target="_blank">LinkedIn</a>
     </span>
-    <p>© 2019 Cori Beecroft</p>
+    <p>© 2020 Cori Beecroft</p>
 </footer>)
 
 
